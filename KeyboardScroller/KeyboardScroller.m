8a8d0bb3a655ddb7e4c2062c41ab76913964a17e
René Dekker <fishinear@hotmail.com>
2018-09-15 15:51:16 +0200

    Small bug fixes

--- KeyboardScroller/KeyboardScroller.m
+++ KeyboardScroller/KeyboardScroller.m
@@ -3,11 +3,44 @@
 //  Flyskyhy
 //
 //  Created by René Dekker on 2017-11-19.
-//  Copyright © 2017 Renevision. All rights reserved.
+//  Copyright © 2017-2018 Renevision. All rights reserved.
 //
 
 #import "KeyboardScroller.h"
 #import <UIKit/UIKit.h>
+
+// Sequence of notifications that arrive as result of different kinds of actions
+//
+// Action               iPhone 4S iOS-8         iPhone 7+ iOS-10    iPhone X iOS-11
+//
+// select field         fS-kS-wL                fS-kS               fS-kS
+// deselect field       kH-fD                   kH-fD               kH-fD
+// change field         fD-fS-wL                fD-fS-kS            fD-fS
+// rotate phone         wL-kHo-kSn-kHn-kSn      kHo-wL-kSn-kSn      wL-kHo-kSn-kSn
+// change keyboard      kS                      kS                  kS
+// hide enclosing view  fD                      fD-kH               fD-kH
+//
+// Explanation for abbreviations:
+//
+// fS  - fieldSelected (UITextFieldTextDidBeginEditingNotification)
+// fD  - fieldDeselected (UITextFieldTextDidEndEditingNotification)
+// kS  - keyboardWillShow (UIKeyboardWillShowNotification)
+// kH  - keyboardWillHide (UIKeyboardWillHideNotification)
+// wL  - viewcontroller.willLayout (possibly leading to a change in contentInset)
+//
+// As far as we can see, UIKeyboardWillChangeFrameNotification will always and only be sent directly before a Show or Hide notification,
+// and at least a Show notification is always sent, even if we only change keyboard size/type. That makes it easier
+// to ignore the ChangeFrame notification than to support it directly.
+
+@interface UITextField (DoneExtension)
+    - (void) done;
+@end
+@implementation UITextField (DoneExtension)
+    - (void) done
+    {
+        [self endEditing:YES];
+    }
+@end
 
 @implementation KeyboardScroller {
     UITextField *selectedField;
@@ -16,15 +49,12 @@
     UIEdgeInsets originalInsets;
     UIEdgeInsets requiredInsets;
     UINavigationItem *navigationItem;
-    UIToolbar *numberToolbar;
     UIGestureRecognizer *gesture;
 }
 
 - (void) done
 {
-    if (currentScrollView != nil) {
-        [currentScrollView endEditing:YES];
-    }
+    [selectedField endEditing:YES];
 }
 
 - (void) observeValueForKeyPath:(NSString *)keyPath
@@ -32,6 +62,9 @@
                          change:(NSDictionary<NSKeyValueChangeKey,id> *)change
                         context:(void *)context
 {
+    // In iOS-8, when selecting another field, the view layout logic is called. This may set
+    // the contentInset to an incorrect value, because it does not take the keyboard into account.
+    // If that happens, then we need to change it back to what is needed for the keyboard.
     UIScrollView *scrollView = (UIScrollView *) object;
     UIEdgeInsets insets = scrollView.contentInset;
     if (!UIEdgeInsetsEqualToEdgeInsets(requiredInsets, insets)) {
@@ -39,13 +72,13 @@
     }
 }
 
-- (UIToolbar *) createNumberToolbar
+- (UIToolbar *) createNumberToolbarForField:(UITextField *)field
 {
-    numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    UIToolbar *numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
     numberToolbar.barStyle = UIBarStyleDefault;
     numberToolbar.items = @[
                             [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
-                            [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)]
+                            [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:field action:@selector(done)]
                             ];
     numberToolbar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     numberToolbar.tintColor = [[UIApplication sharedApplication] keyWindow].tintColor;
@@ -58,28 +91,24 @@
     if (field == selectedField) {
         return;
     }
-    if (selectedField != nil && numberToolbar != nil) {
-        selectedField.inputAccessoryView = nil;
-        numberToolbar = nil;
-    }
     selectedField = nil;
     if (field != nil) {
+        // if it is not a normal keyboard, and we have not added a Done button to the navigation bar
+        // then add a toolbar with Done button to the keyboard
+        if (navigationItem == nil &&
+            field.keyboardType != UIKeyboardTypeDefault &&
+            field.inputAccessoryView == nil)
+        {
+            field.inputAccessoryView = [self createNumberToolbarForField:field];
+        }
         UIScrollView *view = [self findEnclosingScrollView:field];
         if (view == nil) {
-            // the new field is not enclosed in a scroll view
+            // the new field is not enclosed in a scroll view, ignore it
             [self detachScrollView];
             return;
         }
         selectedField = field;
         [self attachScrollView:view];
-        // if it is not a normal keyboard, and we have not added a Done button to the navigation bar
-        // then add a toolbar with Done button to the keyboard
-        if (navigationItem == nil &&
-            selectedField.keyboardType != UIKeyboardTypeDefault &&
-            selectedField.inputAccessoryView == nil)
-        {
-            selectedField.inputAccessoryView = [self createNumberToolbar];
-        }
     }
 }
 
@@ -89,6 +118,9 @@
         return;
     }
     [self detachScrollView];
+    if (scrollView == nil) {
+        return;
+    }
     oldPagingEnabled = scrollView.pagingEnabled;
     if (scrollView.pagingEnabled) {
         scrollView.pagingEnabled = NO;
@@ -145,6 +177,22 @@
     do {
         view = view.superview;
     } while (view != nil && ![view isKindOfClass:[UIScrollView class]]);
+    // BUG-FIX V6.6
+    if ([view.superview isKindOfClass:[UITableView class]]) {
+        // UITableView has an internal hidden UIScrollView before iOS 11
+        // return the UITableView itself instead
+        view = view.superview;
+    }
+    // END BUG-FIX
+    // BUG-FIX 6.9: assume each tableview has a UITableViewController which does all the keyboard handling
+    // itself already
+    if ([view isKindOfClass:[UITableView class]]) {
+        // BUG-FIX 6.11: not all UITableViews have UITableViewControllers. Check it explicitly
+        UITableView *tableView = (UITableView *) view;
+        if ([tableView.delegate isKindOfClass:[UITableViewController class]]) {
+            return nil;
+        }
+    }
     return (UIScrollView *)view;
 }
 
@@ -175,7 +223,9 @@
 
 - (void) keyboardWillHide:(NSNotification *)aNotification
 {
-    if (selectedField == nil) {
+    // BUG-FIX V6.6: always detach, even when field is already deselected
+    // if (selectedField == nil) {
+    if (currentScrollView == nil) {
         return;
     }
     [self keyboardWillChangeFrame:aNotification];
@@ -202,8 +252,7 @@
     
     CGRect rect = [currentScrollView convertRect:selectedField.bounds fromView:selectedField];
     // set some margin
-    rect.origin.y -= 5;
-    rect.size.height += 20;
+    rect = CGRectInset(rect, 0, -10);
     
     // scroll the view with the same animation as the keyboard
     [UIView beginAnimations:nil context:NULL];
